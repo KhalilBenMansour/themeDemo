61a29f2a9b240f3472b65b5d11dd64ab3e97d1a1
KhalilBenMansour <khalil.bn.mansour@gmail.com>
2023-11-21 12:32:17 +0100

    Add and styles content in posts

--- index.php
+++ index.php
@@ -1,3 +1,70 @@
 <?php get_header(); ?>
-this is test content
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title">This Our Title 1</h3>
+                <span class="post-author">Khalil</span>
+                <span class="post-date">11/21/2023</span>
+                <span class="post-comments">20 comments</span>
+                <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
+                <p class="post-content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
+                </p>
+                <hr>
+                <p class="categories">
+                    html, testing, coding, test
+                </p>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title">This Our Title 2</h3>
+                <span class="post-author">Khalil</span>
+                <span class="post-date">11/21/2023</span>
+                <span class="post-comments">20 comments</span>
+                <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
+                <p class="post-content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
+                </p>
+                <hr>
+                <p class="categories">
+                    html, testing, coding, test
+                </p>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title">This Our Title 3</h3>
+                <span class="post-author">Khalil</span>
+                <span class="post-date">11/21/2023</span>
+                <span class="post-comments">20 comments</span>
+                <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
+                <p class="post-content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
+                </p>
+                <hr>
+                <p class="categories">
+                    html, testing, coding, test
+                </p>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title">This Our Title 4</h3>
+                <span class="post-author">Khalil</span>
+                <span class="post-date">11/21/2023</span>
+                <span class="post-comments">20 comments</span>
+                <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
+                <p class="post-content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
+                </p>
+                <hr>
+                <p class="categories">
+                    html, testing, coding, test
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
