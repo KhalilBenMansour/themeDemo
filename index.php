ef8c45ff9b4913ff636d1a8dd8f3edece2ab28c1
KhalilBenMansour <khalil.bn.mansour@gmail.com>
2023-11-22 11:57:43 +0100

    Update design posts with icons

--- index.php
+++ index.php
@@ -4,15 +4,16 @@
         <div class="col-sm-6">
             <div class="main-post">
                 <h3 class="post-title">This Our Title 1</h3>
-                <span class="post-author">Khalil</span>
-                <span class="post-date">11/21/2023</span>
-                <span class="post-comments">20 comments</span>
+                <span class="post-author"><i class="fa-regular fa-user fa-sm" style='color:#999;'></i> Khalil, </span>
+                <span class="post-date"><i class="fa-regular fa-calendar fa-sm" style='color:#999;'></i> 11/21/2023, </span>
+                <span class="post-comments"><i class="fa-regular fa-comments fa-sm" style='color:#999;'></i> 20 comments, </span>
                 <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
                 <p class="post-content">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
                 </p>
                 <hr>
                 <p class="categories">
+                    <i class="fa-solid fa-tags fa-sm" style='color:#999;'></i>
                     html, testing, coding, test
                 </p>
             </div>
@@ -20,15 +21,16 @@
         <div class="col-sm-6">
             <div class="main-post">
                 <h3 class="post-title">This Our Title 2</h3>
-                <span class="post-author">Khalil</span>
-                <span class="post-date">11/21/2023</span>
-                <span class="post-comments">20 comments</span>
+                <span class="post-author"><i class="fa-regular fa-user fa-sm" style='color:#999;'></i> Khalil, </span>
+                <span class="post-date"><i class="fa-regular fa-calendar fa-sm" style='color:#999;'></i> 11/21/2023, </span>
+                <span class="post-comments"><i class="fa-regular fa-comments fa-sm" style='color:#999;'></i> 20 comments, </span>
                 <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
                 <p class="post-content">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
                 </p>
                 <hr>
                 <p class="categories">
+                    <i class="fa-solid fa-tags fa-sm" style='color:#999;'></i>
                     html, testing, coding, test
                 </p>
             </div>
@@ -36,15 +38,16 @@
         <div class="col-sm-6">
             <div class="main-post">
                 <h3 class="post-title">This Our Title 3</h3>
-                <span class="post-author">Khalil</span>
-                <span class="post-date">11/21/2023</span>
-                <span class="post-comments">20 comments</span>
+                <span class="post-author"><i class="fa-regular fa-user fa-sm" style='color:#999;'></i> Khalil, </span>
+                <span class="post-date"><i class="fa-regular fa-calendar fa-sm" style='color:#999;'></i> 11/21/2023, </span>
+                <span class="post-comments"><i class="fa-regular fa-comments fa-sm" style='color:#999;'></i> 20 comments, </span>
                 <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
                 <p class="post-content">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
                 </p>
                 <hr>
                 <p class="categories">
+                    <i class="fa-solid fa-tags fa-sm" style='color:#999;'></i>
                     html, testing, coding, test
                 </p>
             </div>
@@ -52,19 +55,21 @@
         <div class="col-sm-6">
             <div class="main-post">
                 <h3 class="post-title">This Our Title 4</h3>
-                <span class="post-author">Khalil</span>
-                <span class="post-date">11/21/2023</span>
-                <span class="post-comments">20 comments</span>
+                <span class="post-author"><i class="fa-regular fa-user fa-sm" style='color:#999;'></i> Khalil, </span>
+                <span class="post-date"><i class="fa-regular fa-calendar fa-sm" style='color:#999;'></i> 11/21/2023, </span>
+                <span class="post-comments"><i class="fa-regular fa-comments fa-sm" style='color:#999;'></i> 20 comments, </span>
                 <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
                 <p class="post-content">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
                 </p>
                 <hr>
                 <p class="categories">
+                    <i class="fa-solid fa-tags fa-sm" style='color:#999;'></i>
                     html, testing, coding, test
                 </p>
             </div>
         </div>
+
     </div>
 </div>
 <?php get_footer(); ?>
