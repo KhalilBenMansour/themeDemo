a5bb8737483bc400d0f3b718529d92a5ba379a2b
KhalilBenMansour <khalil.bn.mansour@gmail.com>
2023-11-23 13:22:22 +0100

    Add comment_popup_link function

--- index.php
+++ index.php
@@ -17,6 +17,10 @@
                         <span class="post-date">
                             <i class="fa-regular fa-calendar fa-sm" style='color:#999;'></i> <?php the_time('F j, Y'); ?>,
                         </span>
+                        <span class="post-comments">
+                            <i class="fa-regular fa-comments fa-sm" style='color:#999;'></i>
+                            <?php comments_popup_link('0 Comments', 'One Comment', '% Comments', 'comment-url', 'Comments disabled') ?>,
+                        </span>
                         <hr>
                         <p class="categories">
                             <i class="fa-solid fa-tags fa-sm" style='color:#999;'></i>
@@ -33,18 +37,12 @@
 
 <!-- <div class="col-sm-6">
             <div class="main-post">
-                <span class="post-author"><i class="fa-regular fa-user fa-sm" style='color:#999;'></i> Khalil, </span>
-                <span class="post-date"><i class="fa-regular fa-calendar fa-sm" style='color:#999;'></i> 11/21/2023, </span>
                 <span class="post-comments"><i class="fa-regular fa-comments fa-sm" style='color:#999;'></i> 20 comments, </span>
                 <img class="img-fluid img-thumbnail" src="https://placehold.co/600x200" alt="">
                 <p class="post-content">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam magni, inventore voluptatem omnis dicta sapiente ipsam maiores saepe id architecto libero adipisci unde repellat ex aut veniam accusantium ratione. Deserunt.
                 </p>
-                <hr>
-                <p class="categories">
-                    <i class="fa-solid fa-tags fa-sm" style='color:#999;'></i>
-                    html, testing, coding, test
-                </p>
+                
             </div>
         </div> -->
 
