7d83067a4b747e0cdb0741855ad667c1ac6feff1
KhalilBenMansour <khalil.bn.mansour@gmail.com>
2023-11-29 12:24:33 +0100

    Add comment form

--- comments.php
+++ comments.php
@@ -1,15 +1,25 @@
 <?php
-if (comments_open()) {
-    echo '<h3>' . comments_number('0 Comments', '1 Comment', '% Comments') . '</h3>';
+if (comments_open()) { ?>
+    <h3 class='comments-count'> <?php comments_number('0 Comments', '1 Comment', '% Comments') ?> </h3>
+<?php
     echo '<ul class="list-unstyled comments-list">';
     $comment_arguments = array(
-        'max_depth' => 3,
-        'type' => 'comment',
-        'avatar_size'       => 64,
+        'max_depth' => 3, //comment level
+        'type' => 'comment', //comment type
+        'avatar_size'       => 64, //avatar size in pixels
 
     );
     wp_list_comments($comment_arguments);
     echo '</ul>';
+    echo '<hr class="comment-separator"/>';
+
+    $commentform_arguments = array(
+        'title_reply' => 'Add Your Comment', // Change Add Comment Text
+        'title_reply_to' => 'Add a Reply To [%s]', // Change Add Reply To Text
+        'class_submit' => 'btn btn-primary btn-md', // Change Submit Button Class
+        'comment_notes_before' // Disable Email Message
+    );
+    comment_form($commentform_arguments);
 } else {
     echo 'Sorry, Comments are disabled';
 }
