e7e28db49f8f36f96e831338c4908613f6062daa
KhalilBenMansour <khalil.bn.mansour@gmail.com>
2023-12-02 12:53:32 +0100

    Display posts use WP_Query

--- author.php
+++ author.php
@@ -71,5 +71,51 @@
 
     </div>
     <!-- End row  -->
+
+    <?php
+    $author_posts_per_page = 4;
+    $author_posts_arguments = array(
+        'author' => get_the_author_meta('ID'),
+        'posts_per_page' => $author_posts_per_page
+    );
+    $author_posts = new WP_Query($author_posts_arguments);
+    if ($author_posts->have_posts()) : ?>
+        <h3 class="author-posts-title"> Latest [<?php echo $author_posts_per_page ?>] posts of <?php the_author_meta('nickname') ?></h3>
+        <?php
+        while ($author_posts->have_posts()) : $author_posts->the_post(); ?>
+            <div class="author-posts">
+                <!-- start row  -->
+                <div class="row">
+                    <div class="col-sm-3">
+                        <?php the_post_thumbnail('', ['class' => 'img-fluid img-thumbnail', 'title' => 'Featured Image']); ?>
+                    </div>
+                    <div class="col-sm-9">
+                        <h3 class="post-title">
+                            <a href=<?php the_permalink() ?>>
+                                <?php the_title(); ?>
+                            </a>
+                        </h3>
+                        <span class="post-date">
+                            <i class="fa-regular fa-calendar fa-sm" style='color:#999;'></i> <?php the_time('F j, Y'); ?>,
+                        </span>
+                        <span class="post-comments">
+                            <i class="fa-regular fa-comments fa-sm" style='color:#999;'></i>
+                            <?php comments_popup_link('0 Comments', 'One Comment', '% Comments', 'comment-url', 'Comments disabled') ?>,
+                        </span>
+                        <div class="post-content">
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row  -->
+            </div>
+            <!-- <div class="clearFix"></div> -->
+    <?php
+        endwhile;
+    endif;
+    wp_reset_postdata();
+    ?>
+
+
 </div>
 <?php get_footer(); ?>
