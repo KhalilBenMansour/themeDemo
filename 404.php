fd5c88389e2bd4f529626a06b78df60de69a4417
KhalilBenMansour <khalil.bn.mansour@gmail.com>
2023-12-05 12:03:56 +0100

    Add content and style of 404 page

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div class="container text-center page-not-found">
+    <img src="<?php echo get_template_directory_uri() . '/imgs/pagenotfound.webp' ?>" alt="Page not found image">
+    <h1>Page Not Found</h1>
+    <p class="lead">Try to search instead</p>
+</div>
+<?php get_footer(); ?>
