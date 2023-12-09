12c78dcdf059588b1a4797839e8c3e0c22abbcc2
KhalilBenMansour <khalil.bn.mansour@gmail.com>
2023-12-09 14:17:35 +0100

    Add two widget posts query in sidebar

--- sidebar-php.php
+++ sidebar-php.php
@@ -27,12 +27,58 @@ $posts_count = $cat->count;
         </div>
     </div>
     <div class="widget">
-        <h3 class="widget-title">Wideget Title</h3>
-        <div class="widget-content">Widget Content</div>
+        <h3 class="widget-title">Latest HTML posts</h3>
+        <div class="widget-content">
+            <ul>
+                <?php
+                $posts_args = array(
+                    'posts_per_page' => 5,
+                    'cat' => 9
+                );
+                $query = new WP_Query($posts_args);
+                if ($query->have_posts()) {
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        echo '<li>';
+                        echo '<a target="_blank" href="';
+                        the_permalink();
+                        echo '">';
+                        the_title();
+                        echo '</a>';
+                        echo '</li>';
+                    }
+                }
+                ?>
+            </ul>
+        </div>
     </div>
     <div class="widget">
-        <h3 class="widget-title">Wideget Title</h3>
-        <div class="widget-content">Widget Content</div>
+        <h3 class="widget-title">Hot posts by Comments</h3>
+        <div class="widget-content">
+            <ul>
+                <?php
+                $hotpost_args = array(
+                    'posts_per_page' => 1,
+                    'orderby' => 'comment_count'
+                );
+                $hotquery = new WP_Query($hotpost_args);
+                if ($hotquery->have_posts()) {
+                    while ($hotquery->have_posts()) {
+                        $hotquery->the_post() ?>;
+                <li>
+                    <a target="_blank" href=" <?php the_permalink() ?>">
+                        <?php the_title() ?>
+                    </a>
+                    <hr>This post has:
+                    <?php comments_popup_link('0 Comments', 'One Comment', '% Comments', 'comment-url', 'Comments disabled') ?>
+                </li>
+
+        <?php
+                    }
+                }
+        ?>
+            </ul>
+        </div>
     </div>
 </div>
 <?php
